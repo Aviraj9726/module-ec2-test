68aafd356f10af71fa6d7c2d5f4754c558d4063a
Avinash <33217478+Aviraj9726@users.noreply.github.com>
2022-06-05 14:18:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "myinstance" {
   ami = data.aws_ami.app_ami
   instance_type = var.instance_type
   key_name = var.key
-  vpc_security_group_ids = [var.security_group.id]
+  
 
 }
 
